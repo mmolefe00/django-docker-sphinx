4479a16f4f340c0a419c3299f0a77a4cbf9a54fd
Molemo Molefe <mmolefe00@gmail.com>
2023-09-05 14:26:51 +0200

    added sphinx docstrings to models.py

--- aot_site/polls/models.py
+++ aot_site/polls/models.py
@@ -7,6 +7,14 @@ from django.db import models
 
 # === QUESTION MODEL === #
 class Question(models.Model):
+    """This class model creates a specific poll question field with an accompanying date
+    and returns the question text. Choices for each question are subsequently created in
+    the :class: 'Choice(models.Model)' class.
+
+    :param models.CharField(max_length=200) question_text: Field with displayed text for each question
+    :param models.DateTimeField('date_published') date_published: Field for the date the question is published
+    """
+
 
     # displays the question as well as the published date
     question_text = models.CharField(max_length=200)  # our primary key
@@ -14,29 +22,27 @@ class Question(models.Model):
 
     # to string to return question
     def __str__(self):
-
+        """to string to return question text"""
         return self.question_text
 
 
 # === CHOICE MODEL === #
 class Choice(models.Model):
+    """This class model creates choice options under a specific question and allows users to vote
+    on said choice options. The specific question stems from :class: 'Question(models.Model)'
 
-    # foreign key from Question class
-    # on_delete=models.CASCADE means that when we delete the poll question, all the choice options delete with it.
+    :param models.ForeignKey() question: The question the choices belong to
+    :param models.CharField(max_length=200) choice_text: Field with displayed text for a single choice option
+    :param models.IntegerField(default=0)  votes: Counts the amount of votes for a single choice option
+    """
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-
-
-    # field for each choice under the question
     choice_text = models.CharField(max_length=200)
-
-
-    # counts all our votes for each choice (under the main question)
-    # if we delete the choice with on_delete, the votes will be auto-deleted.
     votes = models.IntegerField(default=0)
 
-    # to string to return choice text
-    def __str__(self):
 
+    def __str__(self):
+        """to string to return choice text"""
         return self.choice_text
 
 
