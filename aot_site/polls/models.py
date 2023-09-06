a7c6d3abb871ebdd59930fe81c2d799eadf4d43b
Molemo Molefe <mmolefe00@gmail.com>
2023-09-06 15:46:40 +0200

    modified sphinx docstrings in models.py

--- aot_site/polls/models.py
+++ aot_site/polls/models.py
@@ -7,14 +7,15 @@ from django.db import models
 
 # === QUESTION MODEL === #
 class Question(models.Model):
-    """This class model creates a specific poll question field with an accompanying date
+    """This class models a specific poll question field with an accompanying date
     and returns the question text. Choices for each question are subsequently created in
-    the :class: 'Choice(models.Model)' class.
+    the :class:`Choice` model.
 
-    :param models.CharField(max_length=200) question_text: Field with displayed text for each question
-    :param models.DateTimeField('date_published') date_published: Field for the date the question is published
+    :param question_text: Field with displayed text for each question
+    :type question_text: models.CharField(max_length=200)
+    :param date_published: Field for the date the question is published
+    :type date_published: models.DateTimeField('date_published')
     """
-
 
     # displays the question as well as the published date
     question_text = models.CharField(max_length=200)  # our primary key
@@ -22,19 +23,22 @@ class Question(models.Model):
 
     # to string to return question
     def __str__(self):
-        """to string to return question text"""
+        """Return a string representation of the question."""
         return self.question_text
 
 
 # === CHOICE MODEL === #
 class Choice(models.Model):
     """This class model creates choice options under a specific question and allows users to vote
-    on said choice options. The specific question stems from :class: 'Question(models.Model)'
+    on said choice options. The specific question stems from the :class:`~Question` model.
 
-    :param models.ForeignKey() question: The question the choices belong to
-    :param models.CharField(max_length=200) choice_text: Field with displayed text for a single choice option
-    :param models.IntegerField(default=0)  votes: Counts the amount of votes for a single choice option
-    """
+     :param question: The question the choices belong to. Should be a :class:`~Question` instance.
+     :type question: Question
+     :param choice_text: Field with displayed text for a single choice option.
+     :type choice_text: str
+     :param votes: Counts the number of votes for a single choice option.
+     :type votes: int
+     """
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -42,7 +46,7 @@ class Choice(models.Model):
 
 
     def __str__(self):
-        """to string to return choice text"""
+        """Return a string representation of the choice option."""
         return self.choice_text
 
 
