14e52a349f0d94efca3950b78b8b3895e27990e7
Molemo Molefe <mmolefe00@gmail.com>
2023-09-06 15:45:40 +0200

    added sphinx docstrings to views.py

--- aot_site/polls/views.py
+++ aot_site/polls/views.py
@@ -19,16 +19,40 @@ from .models import Question, Choice
 
 # === index / landing page ===
 def index(request):
-    """This function is the index/ landing page of the site which is rendered from the index.html file."""
+    """This function displays the landing page of the site.
+    It renders the index.html template and returns it as a response.
+
+    :param request: A Django HttpRequest object representing the user's request.
+    :type request: HttpRequest
+
+    :return: A Django HttpResponse object containing the rendered "index.html" template.
+    :rtype: HttpResponse
+    """
 
     return render(request, "index.html")
 
 
 # === register as user ===
 def register(request):
-    """This function facilitates the registration of a new user, authenticates them
-    and then redirects them to the home page.
+    """This function facilitates the registration of a new user based on a POST request; then authenticates them
+    and redirects them to the home page. If the request is not successful, the user will be prompted to start again
+    with a clean registration form.
+
+    :param HttpRequest request: The HTTP request object.
+
+    :return: A response containing either a registration form or a redirect to the home page.
+    :rtype: HttpResponse
+
+    :raises: None
+
+    :example:
+        To register a new user, make a POST request to this view with the necessary form data.
+
+    :note:
+        This view assumes that you have a Django `UserCreationForm` for user registration.
+        It also assumes that you have configured URL routing with a 'polls:home' reverse URL.
     """
+
     if request.method != 'POST':
         # return blank form if user enters incorrectly
         form = UserCreationForm()
@@ -57,8 +81,14 @@ def register(request):
 
 # === login as user ===
 def user_login(request):
-    """This function facilitates the login of an existing user, authenticates them
-    and then redirects them to the home page."""
+    """Facilitates the login of an existing user, authenticates and then redirects them to the home page.
+
+    :param request: The HTTP request object.
+    :type request: HttpRequest
+
+    :return: A rendered HTML page for user login or a redirection to the home page upon successful login.
+    :rtype: HttpResponse
+    """
 
     if request.method != 'POST':
         form = AuthenticationForm()
@@ -79,7 +109,15 @@ def user_login(request):
 
 # === home page ===
 def home(request):
-    """This function is the home page displayed after login/registration which is rendered from the home.html file."""
+    """This function is the home page displayed after login/registration
+    It renders the home.html template and returns it as a response.
+
+    :param request: A Django HttpRequest object representing the user's request.
+    :type request: HttpRequest
+
+    :return: A Django HttpResponse object containing the rendered "home.html" template.
+    :rtype: HttpResponse
+    """
 
     return render(request, "home.html")
 
@@ -90,8 +128,14 @@ def home(request):
 
 # === list page ===
 def polls(request):
-    """This function displays a list of poll questions to the user in order of date published.
-    It is rendered from the poll.html file in the templates folder.
+    """Displays a list of poll questions retrieved from the database to the user, in order of date published,
+    and renders the 'poll.html' template
+
+    :param request: The HTTP request object.
+    :type request: HttpRequest
+
+    :return: The rendered HTML response containing the list of poll questions.
+    :rtype: HttpResponse
     """
 
     latest_question_list = Question.objects.order_by('date_published')
@@ -101,8 +145,17 @@ def polls(request):
 
 # === poll detail view ===
 def detail(request, question_id):
-    """This function displays the detail view of a particular question to the user with choice options.
-    Users can then vote for a particular choice. It is rendered from the detail.html file in the templates folder.
+    """Displays the detail view of a specific question to the user, along with its choice options.
+    Users can then vote for a particular choice.
+
+    :param request: The HTTP request object.
+    :type request: HttpRequest
+
+    :param question_id: The unique identifier of the question.
+    :type question_id: int
+
+    :return: The rendered detail page for the question.
+    :rtype: HttpResponse
     """
 
     # displays the detail view of the question being asked using detail.html file in templates folder
@@ -112,9 +165,16 @@ def detail(request, question_id):
 
 # === poll vote view ===
 def vote(request, question_id):
-    """This function fetches the question and corresponding choice options and facilitates the voting
-    process. It is rendered from the detail.html file and redirects users to the results page
-    (facilitated by the results function)
+    """
+    This function facilitates the voting process for a given question and redirects users to the results page.
+
+    :param request: The HTTP request object.
+    :type request: django.http.HttpRequest
+    :param question_id: The ID of the question to vote on.
+    :type question_id: int
+
+    :returns: HttpResponseRedirect to the results page.
+    :rtype: django.http.HttpResponseRedirect
     """
 
     # displays page for user to vote on question with an array of choice options
@@ -142,9 +202,20 @@ def vote(request, question_id):
 
 # === poll results view ===
 def result(request, question_id):
-    """This function displays the poll results of a particular question to the user.
-    It is rendered from the poll.html file in the templates folder.
+    """Displays the poll results of a particular question to the user.
+    It takes a `request` object and a `question_id` as parameters
+    and renders a template to display the results of the specified question.
+
+    :param request: The HTTP request object.
+    :type request: HttpRequest
+
+    :param question_id: The ID of the question to display results for.
+    :type question_id: int
+
+    :return: A rendered HTML page displaying the poll results.
+    :rtype: HttpResponse
     """
+
     # displays the results of our question using results.html file in templates folder
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'results.html', {'question': question})
@@ -155,8 +226,16 @@ def result(request, question_id):
 # ------------------------------------
 
 def store(request):
-    """This function displays the online store to the user. It is rendered from the online_store.html file
-    in the templates folder. The store is not functional yet."""
+    """This function displays the online store to the user. The store is not functional yet.
+    It renders the online_store.html template and returns it as a response.
+
+    :param request: A Django HttpRequest object representing the user's request.
+    :type request: HttpRequest
+
+    :return: A Django HttpResponse object containing the rendered "online_store.html" template.
+    :rtype: HttpResponse
+    """
+
     return render(request, "online_store.html")
 
 # === END === #
